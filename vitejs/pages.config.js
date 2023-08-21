ef07522a55960b5795e6175ea54987fbbe8bfcc0
Alex Gerber <alex-gerber@mail.ru>
2023-08-21 13:31:26 +0300

    added seo tags. ya metrik

--- vitejs/pages.config.js
+++ vitejs/pages.config.js
@@ -3,6 +3,10 @@ import { resolve } from "path";
 const pages = [
   { name: "main", path: resolve(__dirname, "../index.html") },
   { name: "users", path: resolve(__dirname, "../html/policy.html") },
+  {
+    name: "users",
+    path: resolve(__dirname, "../html/yandex_4bcf7c858d70177e.html"),
+  },
 ];
 
 export default pages;
